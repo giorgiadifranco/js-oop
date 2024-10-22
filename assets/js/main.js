6f4b0925390540fa804518b06427337892da2cdd
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-22 15:01:56 +0200

    add class veicolo , constructor, method

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
-console.log('it works');
+//console.log('it works');
+
+
+//classe Veicolo 
+class Veicolo {
+
+    marca;
+    anno;
+    colore;
+    porte;
+
+    constructor (marca, anno, colore, porte = automobile){
+        this.marca = marca
+        this.anno = anno
+        this.colore = colore
+        this.porte = porte
+    }
+
+    getFullName(){
+        return this.marca + ' ' + this.anno+ ' '+this.colore+' '+this.porte
+    }
+
+}
+
+const panda = new Veicolo ('fiat', 2019, 'blu', 3);
+
+
+console.log(panda.getFullName());
+
